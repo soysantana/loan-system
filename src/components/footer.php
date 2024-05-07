4ee803979f956cf1b0c141fd0de7b502776570b3
Arturo Santana <arturo.santtana@gmail.com>
2024-05-07 10:55:35 +0000

    ⚡ add: interface components

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,7 +1,7 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Santana</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Arturo Santana</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       Designed by <a href="">Arturo Santana</a>
--- src/components/header.php
+++ src/components/header.php
@@ -5,7 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>
+    <?php if (!empty($page_title)) echo remove_junk($page_title);
+    elseif(!empty($user))
+    echo ucfirst($user['name']);
+    else echo "Login";?>
+  </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
