761e11b0e037455e3427db30f6b84509a2f46012
Santana <arturo.santtana@gmail.com>
2024-05-14 09:15:25 -0400

    ⚡ chocs: added changes
    Co-Authored-By: Arturo Santana <santana.inffo@gmail.com>

--- src/components/sidebar.php
+++ src/components/sidebar.php
@@ -6,7 +6,7 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="<?php echo url('/src/pages/index.php'); ?>">
           <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
+          <span>Panel de control</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
@@ -23,19 +23,19 @@
         </ul>
       </li><!-- End clients Nav -->
 
-      <li class="nav-heading">Pages</li>
+      <li class="nav-heading">Paginas</li>
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="<?php echo url('/src/pages/user-profile.php'); ?>">
           <i class="bi bi-person"></i>
-          <span>Profile</span>
+          <span>Perfil</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="<?php echo url('/src/pages/register.php'); ?>">
           <i class="bi bi-card-list"></i>
-          <span>Register</span>
+          <span>Registar</span>
         </a>
       </li><!-- End Register Page Nav -->
 
@@ -44,36 +44,5 @@
   </aside><!-- End Sidebar-->
 
 <script>
-var currentUrl = window.location.pathname;
-var actions = {
-  "/src/pages/new-client.php": function () {
-    activateMenuItem("#clients-nav .new-client");
-    expandMenuItem(".sub");
-  },
-  "/src/pages/index.php": function () {
-    expandMenuItem(".nav-link[href='/src/pages/index.php']");
-  },
-  "/src/pages/user-profile.php": function () {
-    expandMenuItem(".nav-link[href='/src/pages/user-profile.php']");
-  },
-};
 
-if (actions[currentUrl]) {
-  actions[currentUrl]();
-}
-
-function activateMenuItem(selector) {
-  document.querySelector(selector).classList.add("active");
-}
-
-function expandMenuItem(selector) {
-  var element = document.querySelector(selector);
-  if (element) {
-    element.classList.remove("collapsed");
-    var navContent = element.nextElementSibling;
-    if (navContent && navContent.classList.contains("nav-content")) {
-      navContent.classList.add("show");
-    }
-  }
-}
 </script>
